3bb5dcc2156a8c15eaee84c9b66b2d6c818d2b63
Decoder07 <pushpam@100ms.live>
2022-08-18 12:46:15 +0530

    Updated flutter 3 dependencies

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -12,7 +12,6 @@ import 'package:hmssdk_flutter_example/common/util/utility_function.dart';
 import 'package:hmssdk_flutter_example/enum/meeting_flow.dart';
 import 'package:hmssdk_flutter_example/hls-streaming/util/hls_title_text.dart';
 import 'package:hmssdk_flutter_example/preview/preview_details.dart';
-import 'package:hmssdk_flutter_example/qr_code_screen.dart';
 import 'package:provider/provider.dart';
 import 'package:wakelock/wakelock.dart';
 import 'package:package_info_plus/package_info_plus.dart';
@@ -129,7 +128,7 @@ class _HomePageState extends State<HomePage> {
 
   void getData() async {
     meetingLinkController.text =
-        "https://decoder.app.100ms.live/preview/xno-jwn-phi";
+        "https://yogi-livestreamingkit.app.100ms.live/meeting/xuq-zjx-ovh";
     int index = await Utilities.getIntData(key: 'mode');
     mode[index] = true;
     mode[1 - index] = false;
@@ -201,119 +200,6 @@ class _HomePageState extends State<HomePage> {
       onWillPop: _closeApp,
       child: SafeArea(
         child: Scaffold(
-            // appBar: AppBar(
-            //   backgroundColor: isDarkMode ? Colors.black : Colors.white,
-            //   elevation: 0,
-            //   title: Text(
-            //     '100ms',
-            //     style: GoogleFonts.inter(color: iconColor),
-            //   ),
-            //   actions: [
-            //     IconButton(
-            //         onPressed: () {
-            //           if (isDarkMode) {
-            //             HMSExampleApp.of(context).changeTheme(ThemeMode.light);
-            //           } else {
-            //             HMSExampleApp.of(context).changeTheme(ThemeMode.dark);
-            //           }
-            //         },
-            //         icon: isDarkMode
-            //             ? SvgPicture.asset(
-            //                 'assets/icons/light_mode.svg',
-            //                 color: iconColor,
-            //               )
-            //             : SvgPicture.asset(
-            //                 'assets/icons/dark_mode.svg',
-            //                 color: iconColor,
-            //               )),
-            //     PopupMenuButton<int>(
-            //       onSelected: handleClick,
-            //       icon: SvgPicture.asset(
-            //         'assets/icons/settings.svg',
-            //         color: iconColor,
-            //       ),
-            //       itemBuilder: (BuildContext context) {
-            //         return [
-            //           PopupMenuItem(
-            //             child: Row(
-            //               mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            //               children: [
-            //                 if (skipPreview)
-            //                   Text("Enable Preview",
-            //                       style: GoogleFonts.inter(color: iconColor))
-            //                 else
-            //                   Text(
-            //                     "Disable Preview",
-            //                     style: GoogleFonts.inter(color: Colors.blue),
-            //                   ),
-            //                 if (skipPreview)
-            //                   SvgPicture.asset(
-            //                       'assets/icons/preview_state_on.svg',
-            //                       color: iconColor)
-            //                 else
-            //                   SvgPicture.asset(
-            //                     'assets/icons/preview_state_off.svg',
-            //                     color: Colors.blue,
-            //                   ),
-            //               ],
-            //             ),
-            //             value: 1,
-            //           ),
-            //           PopupMenuItem(
-            //             child: Row(
-            //               mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            //               children: [
-            //                 if (mirrorCamera)
-            //                   Text("Disable Mirroring",
-            //                       style: GoogleFonts.inter(color: Colors.blue))
-            //                 else
-            //                   Text(
-            //                     "Enable Mirroring",
-            //                     style: GoogleFonts.inter(color: iconColor),
-            //                   ),
-            //                 Icon(
-            //                   Icons.camera_front,
-            //                   color: mirrorCamera ? Colors.blue : iconColor,
-            //                 ),
-            //               ],
-            //             ),
-            //             value: 2,
-            //           ),
-            //           PopupMenuItem(
-            //             child: Row(
-            //               mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            //               children: [
-            //                 if (showStats)
-            //                   Text("Disable Stats",
-            //                       style: GoogleFonts.inter(color: Colors.blue))
-            //                 else
-            //                   Text(
-            //                     "Enable Stats",
-            //                     style: GoogleFonts.inter(color: iconColor),
-            //                   ),
-            //                 SvgPicture.asset(
-            //                   'assets/icons/stats.svg',
-            //                   color: showStats ? Colors.blue : iconColor,
-            //                 ),
-            //               ],
-            //             ),
-            //             value: 3,
-            //           ),
-            //           PopupMenuItem(
-            //             child: Row(
-            //               mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            //               children: [
-            //                 Text("Version ${_packageInfo.version}",
-            //                     style: GoogleFonts.inter(color: iconColor)),
-            //               ],
-            //             ),
-            //             value: 4,
-            //           ),
-            //         ];
-            //       },
-            //     ),
-            //   ],
-            // ),
             body: Center(
           child: SingleChildScrollView(
             child: Column(
@@ -368,30 +254,6 @@ class _HomePageState extends State<HomePage> {
                               height: 1.5,
                               fontSize: 14,
                               fontWeight: FontWeight.w400)),
-                      // ToggleButtons(
-                      //     key: Key('mode_toggle_button'),
-                      //     selectedColor: hmsdefaultColor,
-                      //     selectedBorderColor: hmsdefaultColor,
-                      //     borderRadius: BorderRadius.circular(10),
-                      //     textStyle: GoogleFonts.inter(
-                      //         color: defaultColor,
-                      //         fontSize: 12,
-                      //         fontWeight: FontWeight.w600),
-                      //     children: [Text(" Meeting "), Text("HLS")],
-                      //     onPressed: (int index) {
-                      //       setState(() {
-                      //         for (int buttonIndex = 0;
-                      //             buttonIndex < mode.length;
-                      //             buttonIndex++) {
-                      //           if (buttonIndex == index) {
-                      //             mode[buttonIndex] = true;
-                      //           } else {
-                      //             mode[buttonIndex] = false;
-                      //           }
-                      //         }
-                      //       });
-                      //     },
-                      //     isSelected: mode)
                     ],
                   ),
                 ),
@@ -439,7 +301,7 @@ class _HomePageState extends State<HomePage> {
                   ),
                 ),
                 SizedBox(
-                  height: 16,
+                  height: 20,
                 ),
                 SizedBox(
                   width: width * 0.95,
@@ -483,68 +345,6 @@ class _HomePageState extends State<HomePage> {
                           ),
                         );
                       }),
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                SizedBox(
-                    width: width * 0.95,
-                    child: Divider(
-                      height: 5,
-                      color: dividerColor,
-                    )),
-                SizedBox(
-                  height: 20,
-                ),
-                SizedBox(
-                  width: width * 0.95,
-                  child: ElevatedButton(
-                    style: ButtonStyle(
-                        shadowColor: MaterialStateProperty.all(hmsdefaultColor),
-                        backgroundColor:
-                            MaterialStateProperty.all(hmsdefaultColor),
-                        shape:
-                            MaterialStateProperty.all<RoundedRectangleBorder>(
-                                RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(8.0),
-                        ))),
-                    onPressed: () async {
-                      bool res = await Utilities.getCameraPermissions();
-                      if (res) {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (_) => QRCodeScreen(
-                                      meetingFlow: mode[0]
-                                          ? MeetingFlow.meeting
-                                          : MeetingFlow.hlsStreaming,
-                                    )));
-                      }
-                    },
-                    child: Container(
-                      padding: const EdgeInsets.fromLTRB(16, 12, 16, 12),
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.all(Radius.circular(8))),
-                      child: Row(
-                        mainAxisSize: MainAxisSize.min,
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Icon(
-                            Icons.qr_code,
-                            size: 18,
-                            color: enabledTextColor,
-                          ),
-                          SizedBox(
-                            width: 5,
-                          ),
-                          HLSTitleText(
-                              key: Key("scan_qr_code"),
-                              text: 'Scan QR Code',
-                              textColor: enabledTextColor)
-                        ],
-                      ),
-                    ),
-                  ),
                 ),
               ],
             ),
